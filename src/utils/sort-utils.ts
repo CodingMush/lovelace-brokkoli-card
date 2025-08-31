ea67e17ca9adac3f69228416a3dbae3d4a126d5a
CodingMush <codingmush@de.de>
2025-09-01 01:51:22 +0200

    fix

--- src/utils/sort-utils.ts
+++ src/utils/sort-utils.ts
@@ -3,6 +3,8 @@ import { HomeAssistantEntity } from '../types/brokkoli-list-card-types';
 import { getFieldValue } from './field-definitions';
 import { PHASES } from './constants';
 
+const DATE_FIELDS = ['seeds_start', 'germination_start', 'rooting_start', 'growing_start', 'flowering_start', 'removed_date', 'harvested_date'];
+
 export class SortUtils {
     static getSortedPlants(
         plants: HomeAssistantEntity[],
@@ -14,9 +16,17 @@ export class SortUtils {
             const aValue = getFieldValue(sortColumn, hass, a);
             const bValue = getFieldValue(sortColumn, hass, b);
 
+            if (DATE_FIELDS.indexOf(sortColumn) !== -1) {
+                const aTime = aValue ? Number(aValue) : 0;
+                const bTime = bValue ? Number(bValue) : 0;
+                return sortDirection === 'asc' ? aTime - bTime : bTime - aTime;
+            }
+
             if (sortColumn === 'growth_phase') {
-                const aIndex = PHASES.indexOf(String(aValue).toLowerCase());
-                const bIndex = PHASES.indexOf(String(bValue).toLowerCase());
+                const aPhase = String(aValue).toLowerCase() as typeof PHASES[number];
+                const bPhase = String(bValue).toLowerCase() as typeof PHASES[number];
+                const aIndex = PHASES.indexOf(aPhase);
+                const bIndex = PHASES.indexOf(bPhase);
                 return sortDirection === 'asc' ? aIndex - bIndex : bIndex - aIndex;
             }
 
